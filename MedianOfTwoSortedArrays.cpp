52837583656c13299aa8d23f77ba46630d0fdd60
bly <bly930725@gmail.com>
2014-06-08 11:44:00 +0800

    update Median of Two Sorted Arrays

--- MedianOfTwoSortedArrays.cpp
+++ MedianOfTwoSortedArrays.cpp
@@ -9,8 +9,11 @@ public:
     double findMedianSortedArrays(int A[],int m,int B[],int n) 
     {
         int total=m+n;
-		if(total%2==0) return (double)(findKthSortedArrays(A,m,B,n,total/2)+findKthSortedArrays(A,m,B,n,total/2+1))/2;
-		else return (double)findKthSortedArrays(A,m,B,n,(total+1)/2);
+		if(total%2==0)
+			return (double)(findKthSortedArrays(A,m,B,n,total/2)+
+				findKthSortedArrays(A,m,B,n,total/2+1))/2;
+		else 
+			return (double)findKthSortedArrays(A,m,B,n,(total+1)/2);
     }
     int findKthSortedArrays(int A[],int m,int B[],int n,int k)
 	{
@@ -18,8 +21,10 @@ public:
 		if(m==0) return B[k-1];
 		if(k==1) return min(A[0],B[0]);
 		int pa=min(k/2,m),pb=k-pa;
-		if(A[pa-1]>B[pb-1]) return findKthSortedArrays(A,m,B+pb,n-pb,k-pb);
-		else if(A[pa-1]<B[pb-1]) return findKthSortedArrays(A+pa,m-pa,B,n,k-pa);
+		if(A[pa-1]>B[pb-1])
+			return findKthSortedArrays(A,m,B+pb,n-pb,k-pb);
+		else if(A[pa-1]<B[pb-1]) 
+			return findKthSortedArrays(A+pa,m-pa,B,n,k-pa);
 		else return A[pa-1];
 	}
 };
